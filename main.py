95bad8aa7a0543fbb31ec46b25763eb0b709c426
redfox-23 <34213058+redfox-23@users.noreply.github.com>
2022-07-02 13:33:26 +0200

    Update main.py
    channel id

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("!horoszkop") and message.channel.id == 992728004373524520:
+    if message.content.startswith("!horoszkop") and message.channel.id == CHANNEL_ID:
         try:
             await message.channel.send(horoscope.fetch(message.content.replace("!horoszkop ", "")))
         except Exception:
             await message.channel.send("Nem létező horoszkóp!")
-    elif message.content.startswith("!horoszkop") and not message.channel.id == 992728004373524520:
-        await message.channel.send(f"Kérlek használd a megfelelő csatornát: <#992728004373524520> :)")
+    elif message.content.startswith("!horoszkop") and not message.channel.id == CHANNEL_ID:
+        await message.channel.send(f"Kérlek használd a megfelelő csatornát: <#CHANNEL_ID> :)")
     
     elif message.content.startswith("!latex"):
         latex.save_image_from_latex(message.content.replace("!latex", ""))
