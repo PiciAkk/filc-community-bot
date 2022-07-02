6430f9df24dcd591c111157f78659fc243e4494b
redfox-23 <dezso@dezso.xyz>
2022-07-02 13:18:11 +0200

    horoszkóphoz megfelelő channel használata, http cat és random cat képek; mert macskás képek mindig jól jönnek

---
+++ catto.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+def fetch(catto):
+    image = open("cat.gif", "wb+")
+    if len(catto) <= 3:
+        image.write(requests.get(f"https://http.cat/{catto}").content)
+    elif len(catto) > 3 and catto == "random":
+        image.write(requests.get(requests.get("https://aws.random.cat/meow").json()["file"]).content)
+    else:
+        image.write(requests.get(f"https://http.cat/404").content)
+    image.close()
--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import discord
 import latex
 import horoscope
+import catto
 
 client = discord.Client()
 token = open("token.txt", "r").read()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("!horoszkop"):
+    if message.content.startswith("!horoszkop") and message.channel.id == 992728004373524520:
         try:
             await message.channel.send(horoscope.fetch(message.content.replace("!horoszkop ", "")))
         except Exception:
             await message.channel.send("Nem létező horoszkóp!")
+    elif message.content.startswith("!horoszkop") and not message.channel.id == 992728004373524520:
+        await message.channel.send(f"Kérlek használd a megfelelő csatornát: <#992728004373524520> :)")
+    
     elif message.content.startswith("!latex"):
         latex.save_image_from_latex(message.content.replace("!latex", ""))
         await message.channel.send(file=discord.File("compiled_latex.png"))
+    
+    elif message.content.startswith("!cat"):
+        catto.fetch(message.content.replace("!cat ", ""))
+        await message.channel.send(file=discord.File("cat.gif"))
 
 client.run(token)
