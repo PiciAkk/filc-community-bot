88c23bc52dbfae6921588b2f559cd787d60a2db4
redfox-23 <34213058+redfox-23@users.noreply.github.com>
2022-07-03 14:55:01 +0200

    case sensitive parancsok, szebb horoszkóp kijelzés embeddel

--- horoscope.py
+++ horoscope.py
@@ -1,9 +1,12 @@
 import unidecode
 import requests
+import discord
+import case_sensitive
 
 horoscopes = "kos,bika,ikrek,rak,oroszlan,szuz,merleg,skorpio,nyilas,bak,vizonto,halak".split(",")
 
 def prepare(string):
+    string = case_sensitive.transform(["!horoszkop","! horoszkop"], string)
     return unidecode.unidecode(string).lower()
 
 def get_url(horoscope):
@@ -13,9 +16,11 @@ def get_url(horoscope):
     return f"https://www.astronet.hu/horoszkop/{prepare(horoscope)}-napi-horoszkop/"
 
 def fetch(horoscope):
+    print(f"fetch: {horoscope}")
+
     html = requests.get(get_url(horoscope)).content.decode("utf-8")
 
     start = html.find('<div class="details-content">') + 29
     end = html[start::].find('</div>') + start
 
-    return html[start:end]
+    return discord.Embed(title=f"Horoszkóp - {prepare(horoscope)}", description=html[start:end], color=0x336EFF)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import latex
 import horoscope
 import catto
 
-CHANNEL_ID = 992771006403985429
+HOROSCOPE_CHANNEL_ID = 992728004373524520 #992771006403985429
 
 client = discord.Client()
 token = open("token.txt", "r").read()
@@ -19,27 +19,27 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("!horoszkop") and message.channel.id == CHANNEL_ID:
+    if (message.content.startswith("!horoszkop") or message.content.startswith("! horoszkop")) and message.channel.id == HOROSCOPE_CHANNEL_ID:
         try:
-            await message.channel.send(horoscope.fetch(message.content.replace("!horoszkop ", "")))
+            await message.channel.send(embed=horoscope.fetch(message.content))
         except Exception:
             await message.channel.send("Nem létező horoszkóp!")
 
-    elif message.content.startswith("!horoszkop") and not message.channel.id == CHANNEL_ID:
-        await message.channel.send(f"Kérlek használd a megfelelő csatornát: <#{CHANNEL_ID}> :)")
+    elif (message.content.startswith("!horoszkop") or message.content.startswith("! horoszkop")) and not message.channel.id == HOROSCOPE_CHANNEL_ID:
+        await message.channel.send(f"Kérlek használd a megfelelő csatornát: <#{HOROSCOPE_CHANNEL_ID}> :)")
     
-    elif message.content.startswith("!latex"):
+    elif message.content.startswith("!latex") or message.content.startswith("! latex"):
         try:
-            latex.save_image_from_latex(message.content.replace("!latex", ""))
+            latex.save_image_from_latex(message.content)
             await message.channel.send(file=discord.File("compiled_latex.png"))
         except Exception:
             await message.channel.send("Érvénytelen LaTeX!")
     
-    elif message.content.startswith("!cat"):
-        catto.fetch(message.content.replace("!cat ", ""))
+    elif message.content.startswith("!cat") or message.content.startswith("! cat"):
+        catto.fetch(message.content)
         await message.channel.send(file=discord.File("cat.gif"))
 
-    elif message.content == "!gamer":
+    elif message.content == "!gamer" or message.content == "! gamer":
         await message.author.add_roles(discord.utils.get(message.guild.roles, name="gaymer"))
 
 client.run(token)
